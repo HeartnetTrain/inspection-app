de0cc065711416cb6eb15b484d119282a5db4c4d
aqcaz <wael.messaoud@dedalus.eu>
2023-10-19 00:48:38 +0200

    add lite-server and update sample

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.6.
 
+## Requirements
+
+Make sure to have a minimum Node.js version of either v14.20, v16.14 or v18.10.
+
 ## Development server
 
 - After cloning the project, run npm i on your local machine.
 
 - Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if
-  you
-  change any of the source files.
+  you change any of the source files.
+
+## Simple Deployment from disk
+
+From your project root :
+
+- Run `ng build --watch` on one terminal
+- Run `npm run deploy` on another terminal
 
 ## Running unit tests
 
--- src/modules/main/inspections/screen/inspections.sample.ts
+++ src/modules/main/inspections/screen/inspections.sample.ts
@@ -4,24 +4,24 @@ export class InspectionsSample {
     public static getInspections(): Inspection [] {
         return [
             {
-                "name": "Inspection Paris 16eme",
-                "installationType": "Pipeline",
+                "name": "Inspection 1",
+                "installationType": "Installation 1",
                 "constructionYear": 1951,
-                "company": "GRTgaz",
-                "type": "Bending Strain",
-                "diameter": "40cm",
-                "material": "Steel",
-                "coating": "Poly Ethylene"
+                "company": "Company 1",
+                "type": "Type 1",
+                "diameter": "Diam 1",
+                "material": "Material 1",
+                "coating": "Coating 1"
             },
             {
-                "name": "Inspection Marseille 7eme",
-                "installationType": "Pipeline",
-                "constructionYear": 1967,
-                "company": "Total",
-                "type": "River crossing",
-                "diameter": "47cm",
-                "material": "Cast Iron",
-                "coating": "Epoxy"
+                "name": "Inspection 2",
+                "installationType": "Installation 2",
+                "constructionYear": 2014,
+                "company": "Company 2",
+                "type": "Type 2",
+                "diameter": "Diam 2",
+                "material": "Material 2",
+                "coating": "Coating 2"
             },
         ]
     }
